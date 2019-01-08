444300e24e243aae1e44442a2c14cd026575d3d1
SashaZ <aszhitin@gmail.com>
2019-01-08 19:30:55 +0200

    testng.xml

---
+++ src/test/java/Testpackage/SampleTest.java
@@ -0,0 +1,26 @@
+package Testpackage;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SampleTest {
+
+  @Test (groups = {"1"})
+
+  public void Test1() {
+    System.out.println("Test 1");
+  }
+
+  @Test (groups = {"2"})
+
+  public void Test2() {
+    System.out.println("Test 2");
+    Assert.assertEquals("2", "1");
+  }
+
+  @Test (groups = {"3"})
+
+  public void Test3() {
+    System.out.println("Test 3");
+  }
+}
