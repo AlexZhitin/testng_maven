52765e6d88cc80ff343af34a84e7d22c19ada697
SashaZ <aszhitin@gmail.com>
2019-01-09 18:35:39 +0200

    Priority, DependsOnMethod

---
+++ src/test/java/LearningTestNG/TestNGPrioritization.java
@@ -0,0 +1,29 @@
+package LearningTestNG;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class TestNGPrioritization {
+
+  @Test(priority = 1)
+
+  public void Login(){
+    System.out.println("Login");
+    Assert.assertTrue(1>2);
+  }
+
+
+  @Test(priority = 2)
+
+  public void Navigating(){
+    System.out.println("Navigating");
+  }
+
+  @Test(priority = 3, dependsOnMethods = {"Login", "Navigating"})
+
+  public void Logout(){
+    System.out.println("Log out");
+  }
+
+}
