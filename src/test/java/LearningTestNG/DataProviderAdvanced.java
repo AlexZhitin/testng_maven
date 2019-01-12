03684aef8fa7d41156122b33bedffd7b944a9e1b
AlexZhitin <38076824+AlexZhitin@users.noreply.github.com>
2019-01-12 15:37:47 +0200

    DataProviderAdvanced

--- src/test/java/LearningTestNG/DataProviderAdvanced.java
+++ src/test/java/LearningTestNG/DataProviderAdvanced.java
@@ -1,5 +1,28 @@
 package LearningTestNG;
 
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
 public class DataProviderAdvanced {
 
+
+    @Test(dataProviderClass = DataProviderAdvancedData.class, dataProvider = "datatobeprovided")
+
+    public void registerUser(String firstname, String lastname, String email, String password) {
+        System.out.println(firstname);
+        System.out.println(lastname);
+        System.out.println(email);
+        System.out.println(password);
+
+
+    }
+
+    @Test(dataProviderClass = DataProviderAdvancedData.class, dataProvider = "datatobeprovided")
+
+    public void loginUser(String username, String password) {
+        System.out.println(username);
+        System.out.println(password);
+
+
+    }
 }
--- src/test/java/LearningTestNG/DataProviderAdvancedData.java
+++ src/test/java/LearningTestNG/DataProviderAdvancedData.java
@@ -1,4 +1,49 @@
 package LearningTestNG;
 
+import org.testng.annotations.DataProvider;
+
+import java.lang.reflect.Method;
+
 public class DataProviderAdvancedData {
+
+    @DataProvider(name = "datatobeprovided")
+
+    public static Object[][] datatobeprovided(Method m) {
+        Object[][] object = null;
+
+        if (m.getName().equals("registerUser")) {
+            object = new Object[3][4];
+
+            object[0][0] = "Lara";
+            object[0][1] = "Croft";
+            object[0][2] = "lc@gmail.com";
+            object[0][3] = "tombraider111";
+
+            object[1][0] = "Tom";
+            object[1][1] = "Cruze";
+            object[1][2] = "tc@gmail.com";
+            object[1][3] = "missionimpossible111";
+
+            object[2][0] = "Arnold";
+            object[2][1] = "Schwarzeneger";
+            object[2][2] = "as@gmail.com";
+            object[2][3] = "terminator111";
+        }
+
+        if (m.getName().equals("loginUser")) {
+            object = new Object[3][2];
+
+
+            object[0][0] = "LaraCroft";
+            object[0][1] = "Croftpassword";
+
+            object[1][0] = "TomCruze";
+            object[1][1] = "Cruzepassword";
+
+
+            object[2][0] = "ArnoldSchwarzeneger";
+            object[2][1] = "Schwarzpassword";
+        }
+        return object;
+    }
 }
