818552f98a90e92b80d5369b7fc7ad39e37e45e3
AlexZhitin <38076824+AlexZhitin@users.noreply.github.com>
2019-01-12 22:13:45 +0200

    no message

--- src/test/java/LearningTestNG/ReportsTestNG.java
+++ src/test/java/LearningTestNG/ReportsTestNG.java
@@ -1,4 +1,20 @@
 package LearningTestNG;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class ReportsTestNG {
+
+    @Test
+
+    public void login(){
+        System.out.println("login");
+    }
+
+    @Test
+
+    public void logout(){
+        System.out.println("logout");
+        Assert.assertEquals("white", "black");
+    }
 }
