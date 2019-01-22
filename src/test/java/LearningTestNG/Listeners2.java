12e43f3acab0260c8aab4c73eb8e4ffc3a6aa3b6
AlexZhitin <38076824+AlexZhitin@users.noreply.github.com>
2019-01-22 22:38:26 +0200

    Listeners

--- src/test/java/LearningTestNG/Listeners2.java
+++ src/test/java/LearningTestNG/Listeners2.java
@@ -1,4 +1,16 @@
 package LearningTestNG;
 
-public class Listeners2 {
+import org.testng.ITestResult;
+import org.testng.TestListenerAdapter;
+
+public class Listeners2 extends TestListenerAdapter {
+
+    public void onTestSuccess (ITestResult tr){
+
+        System.out.println("Test passed");
+    }
+
+    public void onTestFailure(ITestResult tr){
+        System.out.println("Test failed");
+    }
 }
