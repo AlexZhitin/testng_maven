f6fede75e4c06fdf1c1819692eac212c2efbea61
AlexZhitin <38076824+AlexZhitin@users.noreply.github.com>
2019-01-24 23:26:13 +0200

    no message

--- src/test/java/ExtentReports/NopCommerceTest.java
+++ src/test/java/ExtentReports/NopCommerceTest.java
@@ -2,82 +2,122 @@ package ExtentReports;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import com.aventstack.extentreports.reporter.configuration.Theme;
+import com.aventstack.extentreports.utils.FileUtil;
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
+import org.testng.ITestResult;
+import org.testng.annotations.*;
 
+import java.io.File;
+import java.io.IOException;
 import java.sql.SQLOutput;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class NopCommerceTest {
 
-  public ChromeDriver driver = new ChromeDriver();
+    public ChromeDriver driver = new ChromeDriver();
 
-  public ExtentHtmlReporter htmlReporter;
-  public ExtentReports extent;
-  public ExtentTest test;
+    public ExtentHtmlReporter htmlReporter;
+    public ExtentReports extent;
+    public ExtentTest test;
 
-  @BeforeTest
-  public void setExtent() { //All report settings are done here
+    @BeforeTest
+    public void setExtent() { //All report settings are done here
 
-    htmlReporter = new ExtentHtmlReporter(System.getProperty("user.dir") + "/test-output/myReport.html");
+        htmlReporter = new ExtentHtmlReporter(System.getProperty("user.dir") + "/test-output/myReport.html");
 
-    htmlReporter.config().setDocumentTitle("Automation Report"); //Title of the report
-    htmlReporter.config().setReportName("Functional Report"); //Name of the report
-    htmlReporter.config().setTheme(Theme.DARK);
+        htmlReporter.config().setDocumentTitle("Automation Report"); //Title of the report
+        htmlReporter.config().setReportName("Functional Report"); //Name of the report
+        htmlReporter.config().setTheme(Theme.DARK);
 
-    extent = new ExtentReports();
-    extent.attachReporter(htmlReporter);
+        extent = new ExtentReports();
+        extent.attachReporter(htmlReporter);
 
-    extent.setSystemInfo("Hostname", "LocalHost");
-    extent.setSystemInfo("Tester Name", "Alex");
-    extent.setSystemInfo("OS", "Windows");
-    extent.setSystemInfo("Browser", "Chrome");
-  }
+        extent.setSystemInfo("Hostname", "LocalHost");
+        extent.setSystemInfo("Tester Name", "Alex");
+        extent.setSystemInfo("OS", "Windows");
+        extent.setSystemInfo("Browser", "Chrome");
+    }
 
-  @AfterTest
+    @AfterTest
 
-  public void endRepot() {
-    extent.flush();
-  }
+    public void endReport() {
+        extent.flush();
+    }
 
-  @BeforeMethod
+    @BeforeMethod
 
-  public void setUp() {
+    public void setUp() {
 
-    driver = new ChromeDriver();
+        driver = new ChromeDriver();
 
-    driver.get("http://demo.nopcommerce.com/");
+        driver.get("http://demo.nopcommerce.com/");
 
-  }
+    }
 
-  @Test
+    @Test
 
-  public void NoCommerceTitletest() {
+    public void NoCommerceTitletest() {
 
-    test = extent.createTest("NoCommerceTitletest");
-    String title = driver.getTitle();
-    System.out.println("title");
-    Assert.assertEquals(title, "nopCommerce demo store");
+        test = extent.createTest("NoCommerceTitletest");
+        String title = driver.getTitle();
+        System.out.println("title");
+        Assert.assertEquals(title, "nopCommerce demo store");
 
-  }
+    }
 
-  @Test
+    @Test
 
-  public void NoCommerceLogoTest() {
+    public void NoCommerceLogoTest() {
 
-    test = extent.createTest("NoCommerceLogoTest");
-    Boolean logo = driver.findElement(By.xpath("//img[@alt='nopCommerce demo store']")).isDisplayed();
-    Assert.assertTrue(logo);
+        test = extent.createTest("NoCommerceLogoTest");
+        Boolean status = driver.findElement(By.xpath("//img[@alt='nopCommerce demo store']")).isDisplayed();
+        Assert.assertTrue(status);
 
-  }
+    }
 
+    @Test
 
+    public void NoCommerceLoginTest() {
+        test = extent.createTest("NoCommerceLoginTest");
+        Assert.assertTrue(true); //test method will be passed
+    }
+
+    @AfterMethod
+
+    public void tearDown(ITestResult result) throws IOException {
+        if (result.getStatus() == ITestResult.FAILURE) {
+            test.log(Status.FAIL, "TEST CASE FAILED IS " + result.getName()); // to add name in extent report
+            test.log(Status.FAIL, "TEST CASE FAILED IS " + result.getThrowable()); // to add error/exception in extent report
+            String screenshotPath = NopCommerceTest.getScreenshot(driver, result.getName());
+            test.addScreenCaptureFromPath(screenshotPath);// adding screen shot
+        } else if (result.getStatus() == ITestResult.SKIP) {
+            test.log(Status.SKIP, "Test Case SKIPPED IS " + result.getName());
+        } else if (result.getStatus() == ITestResult.SUCCESS) {
+            test.log(Status.PASS, "Test Case PASSED IS " + result.getName());
+        }
+        driver.quit();
+    }
+
+    public static String getScreenshot(WebDriver driver, String screenshotName) throws IOException {
+        String dateName = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+        TakesScreenshot ts = (TakesScreenshot) driver;
+        File source = ts.getScreenshotAs(OutputType.FILE);
+
+        // after execution, you could see a folder "FailedTestsScreenshots" under src folder
+        String destination = System.getProperty("user.dir") + "/Screenshots/" + screenshotName + dateName + ".png";
+        File finalDestination = new File(destination);
+        FileUtils.copyFile(source, finalDestination);
+        return destination;
+    }
 }
-
 
