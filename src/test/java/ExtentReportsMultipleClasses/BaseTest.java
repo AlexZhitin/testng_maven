1b88506cf00ed67dc67d751b0282a9da80e37fc1
SashaZ <aszhitin@gmail.com>
2019-01-25 18:33:17 +0200

    Extent Reports multiple classes

---
+++ src/test/java/ExtentReportsMultipleClasses/BaseTest.java
@@ -0,0 +1,4 @@
+package ExtentReportsMultipleClasses;
+
+public class BaseTest {
+}
---
+++ src/test/java/ExtentReportsMultipleClasses/FunctionalTests1.java
@@ -0,0 +1,43 @@
+package ExtentReportsMultipleClasses;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ActualFunctionTest extends BaseTest {
+
+  @Test
+
+  public void NoCommerceTitletest() {
+
+    test = extent.createTest("NoCommerceTitletest");
+    String title = driver.getTitle();
+    System.out.println("title");
+    Assert.assertEquals(title, "nopCommerce demo!!!!!!!!!!!");
+
+  }
+
+  @Test
+
+  public void NoCommerceLogoTest() {
+
+    test = extent.createTest("NopCommerceLogoTest");
+    Boolean status = driver.findElement(By.xpath("//img[@alt='nopCommerce demo store']")).isDisplayed();
+    Assert.assertTrue(status);
+
+  }
+
+  @Test
+
+  public void NoCommerceLoginTest() {
+    test = extent.createTest("NopCommerceLoginTest");
+    test.createNode("Login with valid input");
+    Assert.assertTrue(true);
+
+    test.createNode("Login with invalid input");
+    Assert.assertTrue(true);
+
+
+    Assert.assertTrue(true); //test method will be passed
+  }
+}
