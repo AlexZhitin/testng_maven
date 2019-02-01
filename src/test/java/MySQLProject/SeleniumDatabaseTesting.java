f40585ce6aea464eacb0abe172481f5c81464cc3
SashaZ <aszhitin@gmail.com>
2019-02-01 19:21:02 +0200

    Database testing

--- src/test/java/MySQLProject/SeleniumDatabaseTesting.java
+++ src/test/java/MySQLProject/SeleniumDatabaseTesting.java
@@ -1,9 +1,33 @@
 package MySQLProject;
+
 import java.sql.*;
 
 public class SeleniumDatabaseTesting {
 
-  public static void main (String[] args){
+  public static void main(String[] args) {
 
+    Connection conn = null;
+
+    String url = "jdbc:mysql://localhost:3306/";
+    String databaseName = "btemployee";
+    String username = "root";
+    String password = "lenovos820";
+
+
+    try {
+
+      Class.forName("com.mysql.jdbc.Driver");
+      conn = DriverManager.getConnection(url + databaseName, username, password);
+
+      String sqlQuerry = "";
+      Statement statement =
+
+    } catch (Exception e) {
+      System.out.println("e");
+    } finally {
+      if (conn != null) {
+        conn = null;
+      }
+    }
   }
 }
