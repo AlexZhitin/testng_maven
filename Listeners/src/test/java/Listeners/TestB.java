24af838b0e69c15cb0aefad9ab03f2225f0a631b
AlexZhitin <38076824+AlexZhitin@users.noreply.github.com>
2019-02-23 17:52:35 +0200

    Listeners. Multiclass.

---
+++ Listeners/src/test/java/Listeners/TestB.java
@@ -0,0 +1,34 @@
+package Listeners;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+@Listeners (Listener_Demo.class)
+
+public class TestB {
+
+    WebDriver driver = new ChromeDriver();
+
+    // Test to pass as to verify listeners .
+    @Test
+    public void Login() throws InterruptedException {
+        driver.get("http://demo.guru99.com/V4/");
+        driver.findElement(By.name("uid")).sendKeys("mngr180317");
+        driver.findElement(By.name("password")).sendKeys("YquzanA");
+        driver.findElement(By.name("btnLogin")).click();
+        Thread.sleep(5000);
+        driver.quit();
+    }
+
+    // Forcefully failed this test as to verify listener.
+    @Test
+    public void TestToFail()
+    {
+        System.out.println("This method to test fail");
+        Assert.assertTrue(false);
+    }
+}
