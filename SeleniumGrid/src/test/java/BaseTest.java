7f6a20467a4fbbfe4e55bc38b833a631be3d6a24
AlexZhitin <aszhitin@gmail.com>
2019-09-04 19:24:57 +0300

    no message

---
+++ SeleniumGrid/src/test/java/BaseTest.java
@@ -0,0 +1,43 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import org.testng.ITestNGListener;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+public class BaseTest {
+    //Declare ThreadLocal Driver (ThreadLocalMap) for ThreadSafe Tests
+    protected static ThreadLocal<RemoteWebDriver> driver = new ThreadLocal<>();
+    public CapabilityFactory capabilityFactory = new CapabilityFactory();
+
+    @BeforeMethod
+    @Parameters(value={"browser"})
+    public void setup (String browser) throws MalformedURLException {
+        //Set Browser to ThreadLocalMap
+        driver.set(new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilityFactory.getCapabilities(browser)));
+    }
+
+    public WebDriver getDriver() {
+        //Get driver from ThreadLocalMap
+        return driver.get();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        getDriver().quit();
+    }
+
+    @AfterClass
+    void terminate () {
+        //Remove the ThreadLocalMap element
+        driver.remove();
+    }
+}
---
+++ SeleniumGrid/src/test/java/CapabilityFactory.java
@@ -0,0 +1,13 @@
+import org.openqa.selenium.Capabilities;
+
+public class CapabilityFactory {
+    public Capabilities capabilities;
+
+    public Capabilities getCapabilities (String browser) {
+        if (browser.equals("chrome"))
+            capabilities = OptionsManager.getChromeOptions();
+        else
+            capabilities = OptionsManager.getFirefoxOptions();
+        return capabilities;
+    }
+}
