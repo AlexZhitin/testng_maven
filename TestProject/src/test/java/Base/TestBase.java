71d9c1f61696ab3ae42299b08c424f18690248ad
AlexZhitin <aszhitin@gmail.com>
2019-10-03 16:26:08 +0300

    Added TestModule

--- TestProject/src/test/java/Base/TestBase.java
+++ TestProject/src/test/java/Base/TestBase.java
@@ -3,16 +3,24 @@ package Base;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.*;
+import org.openqa.selenium.safari.SafariDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+
+import java.util.concurrent.TimeUnit;
+
 
 public class TestBase {
 
-  private WebDriver driver;
+  WebDriver driver;
+  public ThreadLocal<WebDriver> dr = new ThreadLocal<>();
+
   private String osname = System.getProperty("os.name");
   private static String driverPath = System.getProperty("user.dir") + "/src/main/resources/";
 
   public WebDriver getDriver() {
-    return driver;
+    return dr.get();
   }
 
   private void setDriver(String browserType, String appURL) {
@@ -23,24 +31,27 @@ public class TestBase {
       case "firefox":
         driver = initFirefoxDriver(appURL);
         break;
+      case "safari":
+        driver = initSafariDriver(appURL);
+        break;
       default:
         System.out.println("browser: " + browserType + " is invalid. Launching Chrome as browser of choice..");
         driver = initChromeDriver(appURL);
-
     }
-
   }
 
   private WebDriver initChromeDriver(String appURL) {
-    System.out.println("Launching google chrome with new profile..");
+    System.out.println("Launching Chrome browser..");
     if (osname.startsWith("Mac OS X")) {
       System.setProperty("webdriver.chrome.driver", driverPath + "chromedriver");
     } else if (osname.startsWith("Windows")) {
       System.setProperty("webdriver.chrome.driver", driverPath + "chromedriver.exe");
     }
     WebDriver driver = new ChromeDriver();
+    setWebDriver(driver);
     driver.manage().window().maximize();
     driver.navigate().to(appURL);
+    driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
     return driver;
   }
 
@@ -53,8 +64,20 @@ public class TestBase {
       System.setProperty("webdriver.gecko.driver", driverPath + "geckodriver.exe");
     }
     WebDriver driver = new FirefoxDriver();
+    setWebDriver(driver);
     driver.manage().window().maximize();
     driver.navigate().to(appURL);
+    driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    return driver;
+  }
+
+  private WebDriver initSafariDriver(String appURL) {
+    System.out.println("Launching Safari browser..");
+    WebDriver driver = new SafariDriver();
+    setWebDriver(driver);
+    driver.manage().window().maximize();
+    driver.navigate().to(appURL);
+    driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
     return driver;
   }
 
@@ -63,14 +86,31 @@ public class TestBase {
   public void initializeTestBaseSetup(String browserType, String appURL) {
     try {
       setDriver(browserType, appURL);
-
     } catch (Exception e) {
       System.out.println("Error....." + e.getStackTrace());
     }
   }
 
+  public void setWebDriver(WebDriver driver){
+    dr.set(driver);
+  }
+
   @AfterMethod
   public void tearDown() {
-    driver.quit();
+    getDriver().quit();
+    dr.set(null);
   }
 }
+
+
+/*@Parameters({"browserType"})
+
+  @AfterMethod
+  public void tearDown(String browserType) throws IOException, InterruptedException {
+    if (browserType.equals("safari")) {
+
+      driver.quit();
+      *//*Runtime.getRuntime().exec("killall Safari");*//*
+    } else {
+      driver.quit();
+    }*/
