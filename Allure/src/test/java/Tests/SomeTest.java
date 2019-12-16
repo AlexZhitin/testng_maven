59282261ee181aa2b85432bb6a633bea18c9fb6a
AlexZhitin <aszhitin@gmail.com>
2019-12-16 13:42:37 +0200

    no message

--- Allure/src/test/java/Tests/SomeTest.java
+++ Allure/src/test/java/Tests/SomeTest.java
@@ -11,8 +11,11 @@ import io.qameta.allure.Story;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
+import org.testng.SkipException;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+import org.testng.internal.TestResult;
 
 public class SomeTest extends TestBase {
   private WebDriver driver;
