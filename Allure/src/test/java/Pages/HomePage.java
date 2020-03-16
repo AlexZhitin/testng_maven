1ba46a5a95fd1ccd2f1d79261df427338fdfef84
AlexZhitin <aszhitin@gmail.com>
2020-03-16 14:29:13 +0200

    no message

--- Allure/src/test/java/Pages/HomePage.java
+++ Allure/src/test/java/Pages/HomePage.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 public class HomePage {
 
-    @FindBy(xpath = "//li[@data-level='1']")
+    @FindBy(xpath = "//li/a/i")
     List<WebElement> sections;
 
     @FindBy(xpath = "//*[@class='item-login']//a")
