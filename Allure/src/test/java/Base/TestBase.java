8b98605ead9a996359bc3ddce71105fbdf03cc63
AlexZhitin <aszhitin@gmail.com>
2020-03-20 18:26:25 +0200

    Works perfect!

--- Allure/src/test/java/Base/TestBase.java
+++ Allure/src/test/java/Base/TestBase.java
@@ -7,7 +7,9 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.safari.SafariDriver;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 
 import java.util.concurrent.TimeUnit;
 
