02ae4424b9ca5ca593a0e1ec31857774de50a390
AlexZhitin <aszhitin@gmail.com>
2020-03-25 21:20:52 +0200

    no message

--- Allure/src/test/java/Base/AllureTestListener.java
+++ Allure/src/test/java/Base/AllureTestListener.java
@@ -98,8 +98,8 @@ public class AllureTestListener implements ITestListener {
         System.out.println("*** Execution of a test method" + result.getMethod().getMethodName() + " of a test class " + result.getMethod().getRealClass().getName() + " failed...");
         Object testClass = result.getInstance();
         WebDriver webDriver = ((TestBase) testClass).getDriver(Boolean.parseBoolean(local));
-        System.out.println("Screenshot captured for test case: " + (result.getMethod().getMethodName()));
         saveScreenshotPNG(webDriver);
+        System.out.println("Screenshot captured for test case: " + (result.getMethod().getMethodName()));
         saveTextLog(result.getMethod().getMethodName() + " failed and screenshot taken!");
     }
 
